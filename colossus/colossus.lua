5b3da07287fa3ae884c8140f2437236313333f1b
Roberto Trugilho Moreira <105746425+Trugilho@users.noreply.github.com>
2023-08-27 15:12:02 -0300

    Add files via upload

---
+++ colossus/colossus.lua
@@ -0,0 +1,12 @@
+local colossus = {}
+
+colossus.name = "Prismarine Colossus"
+colossus.description = "Grande estátua de prismarine que mede perto de 30 metros de altura."
+
+colossus.maxHealth = 28
+colossus.health = 28
+colossus.attack = 5
+colossus.defense = 6
+colossus.speed = 2
+
+return colossus
